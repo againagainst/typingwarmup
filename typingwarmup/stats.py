3398be3182763d95f30874ed32255d9ba45af56a
Sergey Belash <again.against@gmail.com>
2021-05-14 17:30:38 +0200

    better stats

--- typingwarmup/stats.py
+++ typingwarmup/stats.py
@@ -1,4 +1,5 @@
 from collections import defaultdict
+from typing import List, Tuple
 
 import text
 
@@ -15,10 +16,27 @@ class Stats:
 
     def formatted(self) -> str:
         result = ""
-        for exp, act_dict in self.data.items():
-            errors_on_key = sum(act_dict.values())
+        for exp, errors_on_key, act_dict in self.sorted():
             result += text.expected_key_stat.format(exp, errors_on_key)
-            for act, count in act_dict.items():
+            for act, count in act_dict:
                 result += text.actual_key_stat.format(act, count)
             result += "\n"
+        return result
+
+    def sorted(self) -> List[Tuple[str, int, List[Tuple[str, int]]]]:
+        """
+        {
+            a: {b: 1, c: 2},
+            d: {b: 1, c: 1}
+        } -> [
+            (a, 3, [(b, 1), (c, 2)]),
+            (d, 2, [(b, 1), (c, 1)])
+        ]
+        """
+        result = []
+        for exp, act_dict in self.data.items():
+            act_list = sorted(act_dict.items(), key=lambda k: k[1], reverse=True)
+            errors_on_key = sum(map(lambda k: k[1], act_list))
+            result.append((exp, errors_on_key, act_list))
+        result.sort(key=lambda k: k[1], reverse=True)
         return result
